9f5fb9b981878538f2bc78b8d3e0965406c7fae6
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2021-03-03 22:10:37 +0200

    fix: broken build

--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -1,14 +1,13 @@
-import React, { useCallback } from "react";
+import React from "react";
 import firebase from "firebase";
 import { useQuery } from "@apollo/client";
 import { CURRENT_USER } from "../queries";
 
 const SignOutButton = () => {
-  const onSignOut = useCallback(() => firebase.auth().signOut());
   const currentUser = useQuery(CURRENT_USER);
 
   if (currentUser?.data?.currentUser) {
-    return <button onClick={onSignOut}>Sign Out</button>;
+    return <button onClick={firebase.auth().signOut}>Sign Out</button>;
   }
 
   return null;
