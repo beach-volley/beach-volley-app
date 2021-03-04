b0dfa5399ead1cf5ae041e4e2c979f8c92e9df7e
JimiM <jimmy_makinen@hotmail.com>
2021-03-04 13:36:47 +0200

    merge-conflict fix2

--- src/queries.js
+++ src/queries.js
@@ -2,6 +2,57 @@ import { gql } from "@apollo/client";
 
 //Queries
 
+export const CURRENT_USER = gql`
+  {
+    currentUser {
+      id
+    }
+  }
+`;
+
+export const MATCHES_TOTAL_COUNT = gql`
+  {
+    matches {
+      totalCount
+    }
+  }
+`;
+
+export const MATCHES = gql`
+  {
+    matches {
+      edges {
+        node {
+          id
+          location
+          public
+          playerLimit {
+            end {
+              inclusive
+              value
+            }
+            start {
+              inclusive
+              value
+            }
+          }
+          time {
+            end {
+              inclusive
+              value
+            }
+            start {
+              inclusive
+              value
+            }
+          }
+          nodeId
+        }
+      }
+    }
+  }
+`;
+
 export const MATCH_BY_ID = gql`
   query match($id: Int!) {
     match(id: $id) {
@@ -32,46 +83,22 @@ export const MATCH_BY_ID = gql`
   }
 `;
 
-export const MATCHES = gql`{
-  matches {
-    edges {
-      node {
-        id
-        location
-        public
-        playerLimit {
-          end {
-            inclusive
-            value
-          }
-          start {
-            inclusive
-            value
-          }
-        }
-        time {
-          end {
-            inclusive
-            value
-          }
-          start {
-            inclusive
-            value
-          }
-        }
-        nodeId
-      }
-    }
-  }
-}
-`;
-
 //Mutations
 
 export const CREATE_MATCH = gql`
   mutation createMatch($input: CreateMatchInput!) {
     createMatch(input: $input) {
       match {
+        id
+      }
+    }
+  }
+`;
+
+export const UPSERT_USER = gql`
+  mutation {
+    upsertUser(input: {}) {
+      user {
         id
       }
     }
