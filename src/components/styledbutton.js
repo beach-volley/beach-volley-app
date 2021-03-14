acdba8f9860eee40f62ec4f089bee48b3b392a4b
nmenard <niklas.menard@gmail.com>
2021-03-14 14:42:32 +0200

    switched component name to Camel Case

--- src/components/styledbutton.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const StyledButton = styled.button`
-  background-color: ${(props) => props.theme.colors.gorse};
-  filter: drop-shadow(1px 2px 1px #000000);
-  border-radius: 0.5rem;
-`;
