493ca9d207e0a1c54975945b6224aae2504797ee
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2021-03-26 19:21:49 +0200

    fix: sending push notification failed

--- tasks/send-invitation-to-user.js
+++ tasks/send-invitation-to-user.js
@@ -1,7 +1,7 @@
 const admin = require('../server/firebase-admin');
 
 module.exports = async ({ tokens, name, link }) => {
-  admin.messaging.sendAll(tokens.map(token => ({
+  await admin.messaging().sendAll(tokens.map(token => ({
     token,
     notification: {
       title: `${name} kutsui sinut mukaan pelaamaan.`,
